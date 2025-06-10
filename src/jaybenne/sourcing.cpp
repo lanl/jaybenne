6e35e3f5d8e620b232f21b87899d06ce7bdc8a85
Adam M. Dempsey <adempsey@lanl.gov>
2025-06-10 13:31:04 -0600

    npc_min -> dnpc_min

--- src/jaybenne/sourcing.cpp
+++ src/jaybenne/sourcing.cpp
@@ -64,7 +64,7 @@ TaskStatus SourcePhotons(T *md, const Real t_start, const Real dt) {
   // Extract params
   auto &rng_pool = jb_pkg->template Param<RngPool>("rng_pool");
   const int &num_particles = jb_pkg->template Param<int>("num_particles");
-  const Real &npc_min = jb_pkg->template Param<Real>("npc_min");
+  const Real &dnpc_min = jb_pkg->template Param<Real>("dnpc_min");
   const Real &vv = jb_pkg->template Param<Real>("speed_of_light");
   const Real &sb = jb_pkg->template Param<Real>("stefan_boltzmann");
 
@@ -162,7 +162,7 @@ TaskStatus SourcePhotons(T *md, const Real t_start, const Real dt) {
               // introducing supplementary functions/tasks that set these, or even
               // giving downstream codes the opportunity to set these themselves...
               snpc = npc;
-              dnum = std::max(std::round((snpc > actnum) * (snpc - actnum)), npc_min);
+              dnum = std::max(std::round((snpc > actnum) * (snpc - actnum)), dnpc_min);
               sewpc = erad / dnum;
               ntot += static_cast<int>(dnum);
             },
